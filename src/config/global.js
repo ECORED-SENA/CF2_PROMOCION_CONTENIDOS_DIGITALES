fa72458bf3d1ab7cc61c4451d4aeed83deeeace0
elrilacho <80546125+elrilacho@users.noreply.github.com>
2022-05-25 16:03:12 -0500

    Estructura base

--- src/config/global.js
+++ src/config/global.js
@@ -23,14 +23,32 @@ export default {
         nombreRuta: 'tema1',
         icono: 'far fa-file-alt',
         numero: '1',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Prospección de los clientes',
         desarrolloContenidos: true,
         subMenu: [
           {
             icono: 'far fa-file-alt',
             numero: '1.1',
-            titulo: 'Titulo de segundo nivel',
+            titulo: 'Etapas de la prospección',
             hash: 't_1_1',
+          },
+          {
+            icono: 'far fa-file-alt',
+            numero: '1.2',
+            titulo: 'Ciclo de los prospectos',
+            hash: 't_1_2',
+          },
+          {
+            icono: 'far fa-file-alt',
+            numero: '1.3',
+            titulo: 'Segmentación de clientes',
+            hash: 't_1_3',
+          },
+          {
+            icono: 'far fa-file-alt',
+            numero: '1.4',
+            titulo: 'Arquetipos y <em>buyer</em> persona',
+            hash: 't_1_4',
           },
         ],
       },
@@ -39,22 +57,76 @@ export default {
         nombreRuta: 'tema2',
         icono: 'far fa-file-alt',
         numero: '2',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Sistemas de información',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            icono: 'far fa-file-alt',
+            numero: '2.1',
+            titulo: 'Fuentes de información',
+            hash: 't_2_1',
+          },
+          {
+            icono: 'far fa-file-alt',
+            numero: '2.2',
+            titulo: 'Métodos de recolección de información',
+            hash: 't_2_2',
+          },
+          {
+            icono: 'far fa-file-alt',
+            numero: '2.3',
+            titulo: 'Procesamiento de la información',
+            hash: 't_2_3',
+          },
+          {
+            icono: 'far fa-file-alt',
+            numero: '2.4',
+            titulo: 'Protección de datos y <em>habeas</em> data',
+            hash: 't_2_4',
+          },
+        ],
       },
       {
         nombreRuta: 'tema3',
         icono: 'far fa-file-alt',
         numero: '3',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'El producto',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            icono: 'far fa-file-alt',
+            numero: '3.1',
+            titulo: 'Tipos de productos',
+            hash: 't_3_1',
+          },
+          {
+            icono: 'far fa-file-alt',
+            numero: '3.2',
+            titulo: 'Atributos del producto',
+            hash: 't_3_2',
+          },
+        ],
       },
       {
         nombreRuta: 'tema4',
         icono: 'far fa-file-alt',
         numero: '4',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Portafolio de productos',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            icono: 'far fa-file-alt',
+            numero: '4.1',
+            titulo: 'Comunicación del producto',
+            hash: 't_4_1',
+          },
+          {
+            icono: 'far fa-file-alt',
+            numero: '4.2',
+            titulo: '<em>Target</em> comunicacional',
+            hash: 't_4_2',
+          },
+        ],
       },
     ],
     subMenu: [
@@ -98,32 +170,130 @@ export default {
   },
   complementario: [
     {
-      texto: 'Nombre del material',
-      tipo: 'tipo',
-      link: 'https://www.google.com/',
+      texto:
+        'Academia Pragma. (s.f.). Conoce los 5 arquetipos digitales de los colombianos. ',
+      tipo: 'Articulo',
+      link:
+        'https://www.pragma.com.co/academia/conceptos/conoce-los-5-arquetipos-digitales-de-los-colombianos',
     },
     {
-      texto: 'Nombre del material',
-      tipo: 'tipo',
-      descarga: '/downloads/prueba.pdf',
+      texto:
+        'Departamento Adminisrativo de la Función Pública. (2008). Ley 1266 de 2008. ',
+      tipo: 'PDF',
+      descarga: '/downloads/Ley_1266_de_2008.pdf',
+    },
+    {
+      texto: 'Kotler, P., y Armstrong, G. (2013). Fundamentos de Marketing. ',
+      tipo: 'PDF',
+      descarga: '/downloads/Fundamentos del Marketing-Kotler.pdf',
+    },
+    {
+      texto: 'Lane, K., & Kotler, P. (2006). Dirección de Marketing. ',
+      tipo: 'PDF',
+      descarga:
+        '/downloads/libro direccion-de-marketing(kotler-keller_2006).pdf',
+    },
+    {
+      texto:
+        'Negocios En Tu Mundo. (2012). Emprendedores, a diferenciar sus productos. Nada de commodities ',
+      tipo: 'Video',
+      link: 'https://www.youtube.com/watch?v=GGAGkqStMJw',
+    },
+    {
+      texto: 'Negocios En Tu Mundo. (2012). ¿Qué es y qué no es el valor ',
+      tipo: 'Video',
+      link: 'https://www.youtube.com/watch?v=FpnYS2jtKJk',
     },
   ],
   glosario: [
     {
-      termino: 'Término',
-      significado: 'Definición',
+      termino: 'Cliente',
+      significado:
+        'Un cliente es la persona o entidad que accede a un producto o servicio por medio de una transacción financiera (dinero) u otro medio de pago.',
     },
     {
-      termino: '<em>Término</em>',
-      significado: 'Definición',
+      termino: 'Fidelizar',
+      significado:
+        'Una acción dirigida a conseguir que los clientes mantengan relaciones estables y continuadas con la empresa a lo largo del tiempo.',
+    },
+    {
+      termino: 'Mercado de consumo',
+      significado:
+        'El mercado de consumo se compone por todas aquellas personas que adquieren o compran bienes y servicios y luego lo utilizan para satisfacer sus necesidades finales.',
+    },
+    {
+      termino: 'Modelo de negocio',
+      significado:
+        'Un modelo de negocio es una herramienta previa al plan de negocio que te permitirá definir con claridad qué vas a ofrecer al mercado, cómo lo vas a hacer, a quién se lo vas a vender, cómo se lo vas a vender y de qué forma vas a generar ingresos.',
+    },
+    {
+      termino: 'Prospecto',
+      significado:
+        'El prospecto es un cliente potencial que encaja con las características del cliente ideal, dispone de los medios para comprar (dinero) y está autorizado a tomar decisiones de compra.',
+    },
+    {
+      termino: 'Retención de clientes',
+      significado:
+        'Retener clientes sucede cuando una empresa planifica un conjunto de estrategias de fidelización y acciones con la finalidad de mantener a los clientes actuales a largo plazo.',
+    },
+    {
+      termino: 'Segmento de mercado',
+      significado:
+        'Un segmento de mercado es un grupo de consumidores, en su mayoría homogéneo, ya sea por determinadas características o por sus necesidades, los cuales son identificados como un mercado que presenta deseos o hábitos de compra parecidas y que potencialmente responderían similar a la estrategia desarrollada por el mix de marketing.',
     },
   ],
   referencias: [
     {
       referencia:
-        'Zlackman, C. (2012). 100 años de moda. Sistema de Bibliotecas SENA:',
+        'Agüero Cobo. (2014). Estrategia de fidelización de clientes.',
+      link: '',
+    },
+    {
+      referencia:
+        'Alejandro Schnarch. (2013). Marketing para pymes. Alfa Omega. 1a Edición',
+      link: '',
+    },
+    {
+      referencia:
+        'Beltrán Mora, Marola Narcisa, Gerson Damacio Ledesma Álvarez, and Víctor Martín Parrales Carvajal. "El Buyer Persona Como Factor Clave Entre Las Tendencias En Gestión Empresarial." RECIMUNDO: Revista Científica De La Investigación Y El Conocimiento 3.3 (2019): 659-81. Web.',
+      link: '',
+    },
+    {
+      referencia: 'Ley 1266 de 2008',
       link:
-        'https://sena-primo.hosted.exlibrisgroup.com/primo-explore/fulldisplay?docid=sena_aleph000060086&vid=SENA&search_scope=sena_completo&tab=sena_completo&lang=es_ES&context=L',
+        'https://www.funcionpublica.gov.co/eva/gestornormativo/norma_pdf.php?i=34488',
+    },
+    {
+      referencia:
+        'Kotler, P. (1967). Marketing Management: Analysis, Planning and Control. Prentice Hall.',
+      link: '',
+    },
+    {
+      referencia:
+        'Kotler, P. & Sidney J. Levy. (1969). Broadening the Concept of Marketing. Journal of Marketing, January 1969, Vol. 33, Issue 1, pp.10-15. (Winner of the 1969 Alpha Kappa Psi Foundation Award for the best 1969 paper in the Journal of Marketing.)',
+      link: '',
+    },
+    {
+      referencia:
+        'Academia pragma. (2021). Conoce los 5 arquetipos digitales de los colombianos. ',
+      link:
+        'https://www.pragma.com.co/academia/conceptos/conoce-los-5-arquetipos-digitales-de-los-colombianos',
+    },
+    {
+      referencia:
+        'Promove Consultoría. (2012). Atraer fidelizar clientes. Xunta de Galicia. 1a Edición ',
+      link: '',
+    },
+    {
+      referencia:
+        'Superintendencia de Industria y Comercio. Protección de datos personales. ',
+      link: 'https://www.sic.gov.co/tema/proteccion-de-datos-personales',
+    },
+    {
+      referencia:
+        'Valdés, P. (2019). Buyer persona: el factor clave en tu estrategia de marketing y ventas.',
+      link:
+        'https://www.dobuss.es/buyer-persona-factor-clave-estrategia-marketing-ventas/',
     },
   ],
   creditos: {
@@ -141,6 +311,26 @@ export default {
         centro:
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
       },
+      {
+        nombre: 'Paolo Andrés Cantillo Largo',
+        cargo: 'Experto temático',
+        centro: 'Centro de Comercio - Regional Antioquia ',
+      },
+      {
+        nombre: 'Adriana López',
+        cargo: 'Diseñadora instruccional',
+        centro: 'Centro de Gestión Industrial - Regional Distrito Capital',
+      },
+      {
+        nombre: 'Ana Catalina Córdoba Sus',
+        cargo: 'Asesora metodológica',
+        centro: 'Centro de Diseño y Metrología - Regional Distrito Capital',
+      },
+      {
+        nombre: 'Sandra Patricia Hoyos Sepúlveda',
+        cargo: 'Corrección de estilo',
+        centro: 'Centro de Diseño y Metrología - Regional Distrito Capital ',
+      },
     ],
     desarrolloProducto: [
       {
@@ -150,7 +340,7 @@ export default {
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
       },
       {
-        nombre: 'Nombre',
+        nombre: 'Blanca Flor Tinoco Flores',
         cargo: 'Diseño web',
         centro:
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
@@ -162,7 +352,7 @@ export default {
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
       },
       {
-        nombre: 'Nombre',
+        nombre: 'Camilo Ándres Bolaño Rey',
         cargo: 'Desarrollo front-end',
         centro:
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
