7caaa4ef68f1dc67d74fa910e2ecaf6484d65151
elrilacho <80546125+elrilacho@users.noreply.github.com>
2022-06-23 14:12:14 -0500

    Pendiente actividad didactica

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -11,7 +11,7 @@
     p.mb-5 En todo proceso organizacional, se requiere incrementar las ventas de su portafolio de productos de forma planificada y estratégica, satisfaciendo las necesidades de sus clientes de una forma asertiva. En este sentido, es importante que la empresa siga un proceso disciplinado y estructurado desde el momento de reunir la información necesaria del producto o servicio que recibirá el cliente, en el cual se describan los beneficios, atributos, líneas, entre otras características significativas.
     figure
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/5VXdP4i4pa" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/5VXdP4i4pag" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
 </template>
 
